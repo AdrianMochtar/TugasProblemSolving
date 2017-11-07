a294a4ba9c3e5f7bb031431a41193ce3012e9677
AdrianMochtar <mochtaradrian.am@gmail.com>
2017-11-07 20:38:37 +0800

    menambah percabangan,perhitungan dan output

--- Soal05.cpp
+++ Soal05.cpp
@@ -3,4 +3,13 @@ using namespace std;
 
 int main(){
 	int tinggi,berat;
+	cout << "Masukkan Berapa Tinggi Anda = "; cin >> tinggi;
+	cout << "Masukkan Berapa Berat Anda = "; cin >> berat;
+		if (berat<tinggi/2.5){
+			cout << "Underweight";
+		}else if(tinggi/2.5 <= berat && berat <= tinggi/2.3){
+			cout << "Normal";
+		}else if(tinggi/2.3 < berat){
+			cout << "Overweight";
+		}
 }
