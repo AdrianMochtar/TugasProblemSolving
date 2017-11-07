33565f99f4981a7eede8a07a05641c1225860d73
AdrianMochtar <mochtaradrian.am@gmail.com>
2017-11-07 20:22:50 +0800

    membuat perhitungan absolut

--- Soal01.cpp
+++ Soal01.cpp
@@ -4,4 +4,19 @@ using namespace std;
 
 int main(){
 	int x,y,z;
+	
+cout<< "Nilai 1 = ";
+cin>> x;
+	
+cout<< "Nilai 2 = ";
+cin>> y;
+
+z=x-y;
+
+if (z>0)
+cout<< z;
+
+if (z<0)
+cout<<abs (z);
+
 }
