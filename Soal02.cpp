7832676a66edf29b201d329a37049b069f68d382
AdrianMochtar <mochtaradrian.am@gmail.com>
2017-11-07 20:27:27 +0800

    menambah percabangan dan perhitungan

--- Soal02.cpp
+++ Soal02.cpp
@@ -6,4 +6,21 @@ int main(){
 	char bidang;
 	
 	cout << "Pilih Jenis Bidang datar (P/S)"; cin >> bidang;
+	
+	if (bidang=='p'){
+		cout <<"masukkan tinggi = "; 
+			cin >> tinggi;
+		cout << "masukkan lebar = "; 
+			cin >> lebar;
+		luas = tinggi*lebar;
+		cout << "luas persegi panjang adalah = " << luas;
+		
+	}else if (bidang=='s'){
+		cout << "masukkan alas = "; 
+			cin >> alas;
+		cout << "masukkan tinggi = "; 
+			cin >> tinggi;
+		luas = 0.5*alas*tinggi;
+		cout << "luas segitiga adalah = " << luas;
+	}
 }
