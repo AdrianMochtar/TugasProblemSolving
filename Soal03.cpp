bcd5d3188cff2b0047b0224a4bff6df8c3910747
AdrianMochtar <mochtaradrian.am@gmail.com>
2017-11-07 20:32:51 +0800

    menambah percabangan dan perhitungan gaji

--- Soal03.cpp
+++ Soal03.cpp
@@ -5,4 +5,17 @@ int main(){
 	int g = 5000, gjp = 500000;
 	int waktu,gl,ga,gtot,tax;
 	cout << "shift kerja lembur minggu ini = "; cin >> waktu;
+	if (waktu>0){
+		gl=waktu*g;
+		ga=gl*1.5;
+		tax=ga*15/100;
+		gtot=gjp+ga-tax;
+		
+		cout << "Gaji Anda Minggu Ini = Rp." << gtot << endl;
+}else{
+		tax=gjp*15/100;
+		gtot=gjp-tax;
+		
+		cout << "Gaji Anda Minggu Ini = Rp." << gtot << endl;
+	}
 }
